5a24a22612efc6975c0e92943e83b1cdfd48e8f4
yenniesong <songye93@naver.com>
2024-01-03 23:33:21 +0900

    Initial commit

---
+++ src/main/java/com/study/otherwayjwt/OtherwayjwtApplication.java
@@ -0,0 +1,13 @@
+package com.study.otherwayjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtherwayjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtherwayjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/otherwayjwt/OtherwayjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.otherwayjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtherwayjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
