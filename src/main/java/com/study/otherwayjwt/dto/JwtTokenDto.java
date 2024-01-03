f0e6acddf1cfd6bea678fadc9eec37cae17d0d9d
yenniesong <songye93@naver.com>
2024-01-04 03:21:00 +0900

    first changes created classes and packages

---
+++ src/main/java/com/study/otherwayjwt/dto/JwtTokenDto.java
@@ -0,0 +1,14 @@
+package com.study.otherwayjwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@AllArgsConstructor
+public class JwtTokenDto {
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+}
