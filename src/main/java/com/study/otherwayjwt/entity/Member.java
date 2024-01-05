c0a61bb9f0a64551d38939882a0cc5f0cc60e051
yenniesong <songye93@naver.com>
2024-01-06 04:52:01 +0900

    2nd changes created member classe

--- src/main/java/com/study/otherwayjwt/entity/Member.java
+++ src/main/java/com/study/otherwayjwt/entity/Member.java
@@ -1,4 +1,36 @@
 package com.study.otherwayjwt.entity;
 
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.security.Timestamp;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String username;
+    private String profileImg;
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+    @CreationTimestamp
+    private Timestamp createDate;
+
+    @Builder
+    public Member(String email, String password, String username, Authority authority) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.authority = authority;
+    }
 }
